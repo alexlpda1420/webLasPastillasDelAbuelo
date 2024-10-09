065ee140c878558ed79d85d9dd56e8bd65ccab53
Alexis Roldan <a.eroldan@hotmail.com>
2024-10-09 18:20:41 -0300

    backend

--- Backend/server.js
+++ Backend/server.js
@@ -11,20 +11,6 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-// Rutas
-app.post('/api/register', (req, res) => {
-    const { username, password } = req.body;
-    // Aquí deberías agregar la lógica para guardar el usuario en la base de datos
-    console.log('Registro de usuario:', username);
-    res.json({ message: 'Usuario registrado exitosamente' });
-});
-
-app.post('/api/login', (req, res) => {
-    const { username, password } = req.body;
-    // Aquí deberías agregar la lógica para verificar el usuario
-    console.log('Inicio de sesión de usuario:', username);
-    res.json({ message: 'Inicio de sesión exitoso' });
-});
 
 // Iniciar el servidor
 app.listen(PORT, () => {
