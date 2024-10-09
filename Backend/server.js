b9535263c3853d20662942a2b4e8aa743911ee7b
Alexis Roldan <a.eroldan@hotmail.com>
2024-10-09 19:53:05 -0300

    update

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,11 @@
 // backend/server.js
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
 const app = express();
 const PORT = 3000;
 
-// Middleware
 app.use(bodyParser.json());
-app.use(cors());
+
 
 
 // Iniciar el servidor
