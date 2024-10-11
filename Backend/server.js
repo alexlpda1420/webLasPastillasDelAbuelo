8c0cd0c8b739528cdf7e79241f74b5d8a199453b
Alexis Roldan <a.eroldan@hotmail.com>
2024-10-11 12:23:53 -0300

    update

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@
 // mongodb+srv://aeroldan:N4p01420$$@clasemongo.q4wxy.mongodb.net/?retryWrites=true&w=majority&appName=ClaseMongo
 
 
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
